b09cc7b6bf146d3708b4619446d777488e8a805d
premsharma0123 <prem@massmancybergeeks.com>
2021-01-18 18:40:31 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -4,8 +4,54 @@ $(document).ready(function(){
     } , function (){
         $(".bg-bubbles").removeClass('transition');
     })
+
+    // add & Remove class
+    $(".List01").click(function () {
+      if(!$(this).hasClass('List_active'))
+      {    
+          $(".List01.List_active").removeClass("List_active");
+          $(this).addClass("List_active");        
+      }
+    });
+
+    $('.slider1').owlCarousel({
+      loop: true,
+      margin: 0,
+      autoplay: true,
+    //  nav: true,
+      autoplayTimeout: 4000,
+      smartSpeed: 1500,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 1
+          },
+          1000: {
+              items: 1
+          }
+      }
+    });
+    
 })
 
+
+
+
+
+
+
+
+
+function openList(ListName) {
+  var i;
+  var x = document.getElementsByClassName("port_tab-content");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(ListName).style.display = "block";  
+}
 
 
 // ---counter//
@@ -33,3 +79,6 @@ $(document).ready(function() {
       count(0, counter[j], j);
     }
   });
+
+
+  
