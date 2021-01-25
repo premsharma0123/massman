1d6ec461b1100f6de3f035f69945bac1a5abc8ff
premsharma0123 <prem@massmancybergeeks.com>
2021-01-25 16:55:02 +0530

    commmit

--- js/main.js
+++ js/main.js
@@ -159,18 +159,18 @@ function topFunction() {
 
 // ----inspect ---disable------//
 
-$(document).keydown(function(event){
-  if(event.keyCode==123){
-      return false;
-  }
-  else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
-           return false;
-  }
-});
+// $(document).keydown(function(event){
+//   if(event.keyCode==123){
+//       return false;
+//   }
+//   else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
+//            return false;
+//   }
+// });
 
-$(document).on("contextmenu",function(e){        
- e.preventDefault();
-});
+// $(document).on("contextmenu",function(e){        
+//  e.preventDefault();
+// });
 
 
 // ----disable ----ctrl----U---//
