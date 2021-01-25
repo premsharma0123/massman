261ab0cfba2f5fbf7e0d371832413acbdf56eea4
premsharma0123 <prem@massmancybergeeks.com>
2021-01-25 15:11:34 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -155,18 +155,18 @@ function topFunction() {
 
 // ----inspect ---disable------//
 
-// $(document).keydown(function(event){
-//   if(event.keyCode==123){
-//       return false;
-//   }
-//   else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
-//            return false;
-//   }
-// });
+$(document).keydown(function(event){
+  if(event.keyCode==123){
+      return false;
+  }
+  else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
+           return false;
+  }
+});
 
-// $(document).on("contextmenu",function(e){        
-//  e.preventDefault();
-// });
+$(document).on("contextmenu",function(e){        
+ e.preventDefault();
+});
 
 
 // ----end-----here----//
